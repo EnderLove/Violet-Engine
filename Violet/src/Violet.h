3e01d48f84ac5a7540539c64b20a0f3b174788d5
EnderLove <cocpowergameplays@gmail.com>
2026-07-12 18:06:53 -0500

    Loggin system build

--- Violet/src/Violet.h
+++ Violet/src/Violet.h
@@ -2,6 +2,7 @@
 
 // FOR THE USE OF VIOLET APPLICATIONS 
 #include "Violet/Application.h"
+#include "Violet/Log.h"
 
 // ENTRY POINT
 #include "Violet/EntryPoint.h"
--- Violet/src/Violet/EntryPoint.h
+++ Violet/src/Violet/EntryPoint.h
@@ -5,6 +5,12 @@
 extern Violet::Application* Violet::CreateApplication();
 
 int main(int argc, char** argv) {
+	Violet::Log::Init();
+	VT_CORE_WARN("Initialize Log!");
+
+	int a = 10;
+	VT_INFO("HELLO LOG GAAAAAAAAAAA! Var={0}", a);
+
 	auto app = Violet::CreateApplication();
 	app->Run();
 	delete app;
