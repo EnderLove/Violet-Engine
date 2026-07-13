3168f98465d845d756a389247f5cc98854224d26
EnderLove <cocpowergameplays@gmail.com>
2026-07-13 17:15:34 -0500

    lua premake file

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "Violet"
+
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Violet"
+	location "Violet"
+	kind     "SharedLib" -- DLL
+	language "C++"
+
+	targetdir ("bin/"     .. outputDir .. "/%{prj.name}")
+	objdir    ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On" 
+		systemversion "latest"
+
+		defines {
+			"VIOLET_PLATFORM_WINDOWS",
+			"VIOLET_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputDir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "VT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VT_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind     "ConsoleApp" -- EXE
+	language "C++"
+
+	targetdir ("bin/"     .. outputDir .. "/%{prj.name}")
+	objdir    ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include",
+		"Violet/src"
+	}
+
+	links {
+		"Violet"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On" 
+		systemversion "latest"
+
+		defines {
+			"VIOLET_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "VT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VT_DIST"
+		optimize "On"
