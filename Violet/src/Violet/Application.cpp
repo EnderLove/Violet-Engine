6b4b0661ba117dbb8f1508a759211398ea7d4000
EnderLove <cocpowergameplays@gmail.com>
2026-07-14 23:35:54 -0500

    test events

--- Violet/src/Violet/Application.cpp
+++ Violet/src/Violet/Application.cpp
@@ -1,10 +1,14 @@
 #include "Application.h"
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
 
 namespace Violet {
 	Application::Application() {}
 	Application::~Application() {}
 
 	void Application::Run() {
+		WindowResizeEvent e(1980, 720);
+		VT_TRACE(e);
 		while (true);
 	}
 }
