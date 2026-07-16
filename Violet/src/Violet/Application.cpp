2a02149b8ab086b47c9392b2097750cbc5762a2d
EnderLove <cocpowergameplays@gmail.com>
2026-07-15 20:58:38 -0500

    Added PCH

--- Violet/src/Violet/Application.cpp
+++ Violet/src/Violet/Application.cpp
@@ -1,6 +1,10 @@
+#include "vtpch.h"
+
 #include "Application.h"
 #include "Events/ApplicationEvent.h"
+#include "Events/MouseEvent.h"
 #include "Log.h"
+
 
 namespace Violet {
 	Application::Application() {}
@@ -8,7 +12,9 @@ namespace Violet {
 
 	void Application::Run() {
 		WindowResizeEvent e(1980, 720);
+		MouseMovedEvent a(10, 20);
 		VT_TRACE(e);
+		VT_TRACE(a);
 		while (true);
 	}
 }
