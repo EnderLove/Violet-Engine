832d026496aaf185996eefbcca59bcc06200f75c
EnderLove <cocpowergameplays@gmail.com>
2026-07-16 21:22:05 -0500

    Callbacks set for Application

--- Violet/src/Violet/Application.cpp
+++ Violet/src/Violet/Application.cpp
@@ -1,20 +1,39 @@
 #include "vtpch.h"
 
 #include "Application.h"
-#include "Events/ApplicationEvent.h"
+//#include "Events/ApplicationEvent.h"
 #include "Events/MouseEvent.h"
 #include "Log.h"
 
+#include <GLFW/glfw3.h>
 
 namespace Violet {
-	Application::Application() {}
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+	Application::Application() {
+		window_ = std::unique_ptr<Window>(Window::Create()); // Why the cast?
+		window_->SetEventCallback(BIND_EVENT_FN(OnEvent)); // Loads the fn into the window data_ EventCallback
+	}
+
 	Application::~Application() {}
 
+	// OnEvent(Application::this*, Event& e) compiler pov (BIND_EVENT_FN manage the this*)
+	void Application::OnEvent(Event& e) { // This will be called by the window data_ 
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		VT_CORE_INFO("{0}", e);
+	}
+
 	void Application::Run() {
-		WindowResizeEvent e(1980, 720);
-		MouseMovedEvent a(10, 20);
-		VT_TRACE(e);
-		VT_TRACE(a);
-		while (true);
+		while (running_) {
+			glClearColor(0.5f, 0.0f, 1.0f, 1.0f); 
+			glClear(GL_COLOR_BUFFER_BIT);
+			window_->OnUpdate();
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		running_ = false;
+		return true;
 	}
 }
--- Violet/src/Violet/Application.h
+++ Violet/src/Violet/Application.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Core.h"
+#include "Events/Event.h"
+#include "Window.h"
+#include "Events/ApplicationEvent.h" // need vtpch.h (Window.h)
 
  namespace Violet {
 	class VIOLET_API Application {
@@ -9,6 +12,14 @@
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e); // This is what will happen with a certain event
+
+	private:
+		std::unique_ptr<Window> window_;
+		bool running_ = true;
+
+		bool OnWindowClose(WindowCloseEvent& e);
 	};
 
 	// TO BE DEFINE IN CLIENT
