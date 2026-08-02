8bca8fde4f13ffb8d7b509e37123cd813bb8cb0f
EnderLove <cocpowergameplays@gmail.com>
2026-08-02 16:45:38 -0500

    Shader temp class Added

---
+++ Violet/src/Violet/Renderer/Shader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+namespace Violet {
+	class Shader {
+	public:
+		Shader(const std::string& vertexSource, const std::string& fragmentSource);
+		~Shader();
+
+		void Bind();
+		void Unbind();
+
+	private:
+		unsigned int ShaderID_;
+	};
+}
